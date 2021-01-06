fe02aa915c6bff7a18e75b681b5d0071bc001011
xzr <xzregg>
2021-01-06 17:50:10 +0800

    feat: 支持[]访问设置属性

--- objectdict.py
+++ objectdict.py
@@ -7,14 +7,28 @@ class ObjectDict(dict):
     """Makes a dictionary behave like an object, with attribute-style access.
     """
 
+    def __init__(self, *args, **kwargs):
+
+        super(ObjectDict, self).__init__(*args, **kwargs)
+        for k, v in self.__class__.__dict__.items():
+            if not k.startswith('_'):
+                self[k] = v
+
+
     def __getattr__(self, name):
         try:
             return self[name]
         except KeyError:
             raise AttributeError(name)
 
+    def __setitem__(self, name, value):
+        super(ObjectDict, self).__setitem__(name, value)
+        super(ObjectDict, self).__setattr__(name, value)
+
     def __setattr__(self, name, value):
-        self[name] = value
+        super(ObjectDict, self).__setitem__(name, value)
+        super(ObjectDict, self).__setattr__(name, value)
 
     def from_json(self, json_data, json_loads=json.loads):
-        return self.update(json_loads(json_data))
+        self.update(json_loads(json_data))
+        return self
---
+++ test_objectDict.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021-01-06 17:00
+# @Author  : xzr
+# @File    : test_objectDict
+# @Software: PyCharm
+# @Contact : xzregg@gmail.com
+# @Desc    :
+import json
+from unittest import TestCase
+
+from objectdict import ObjectDict
+
+
+class TestObjectDict(TestCase):
+
+    def test_value(self):
+        class SimpleObjectDict(ObjectDict):
+            int_field = 1
+            str_field = 'a'
+            dict_field = {}
+            int2_f: int
+
+        instance = SimpleObjectDict()
+        instance.int_field = 2
+        instance.str_field = 'b'
+        instance.dict_field = {'3': 4}
+        instance.other_filed = 'asda'
+        print(instance.int_field)
+
+        self.assertEqual(SimpleObjectDict().int_field, 1)
+        self.assertEqual(SimpleObjectDict().str_field, 'a')
+        self.assertEqual(SimpleObjectDict().dict_field, {})
+
+        print(SimpleObjectDict(), instance, instance.int_field)
+
+        self.assertEqual(instance.int_field, 2)
+        instance['int_field'] = 5
+        instance.int2_f='basd'
+        self.assertEqual(instance.int_field, 5)
+        print(SimpleObjectDict(), instance, instance.int_field, SimpleObjectDict().from_json(json.dumps(instance)))
+
+        self.assertEqual(instance, SimpleObjectDict().from_json(json.dumps(instance)))
+
+        self.assertEqual(instance.str_field, 'b')
+        self.assertEqual(instance.dict_field, {'3': 4})
+        self.assertEqual(instance.other_filed, 'asda')
+
+        self.assertEqual(instance, json.loads(json.dumps(instance)))
